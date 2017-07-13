0fa0b934d40440ada197ad4e5e43f9c0c30e5d11
webadm <joe.murphy@gmail.com>
2017-07-13 14:51:48 -0400

    JS fixes

--- ads/ads.js
+++ ads/ads.js
@@ -1,13 +1,12 @@
 var is_mobile = /Android|webOS|iPhone|iPad|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+var googletag = googletag || {};
 var nydn_ads = function(ad_tag) {
-        var googletag = googletag || {};
         googletag.cmd = googletag.cmd || [];
         (function() {
         var gads = document.createElement('script');
         gads.async = true;
         var useSSL = 'https:' == document.location.protocol;
-        gads.src = (useSSL ? 'https:' : 'http:') + 
-        '//www.googletagservices.com/tag/js/gpt.js';
+        gads.src = (useSSL ? 'https:' : 'http:') + '//www.googletagservices.com/tag/js/gpt.js';
         var node = document.getElementsByTagName('script')[0];
         node.parentNode.insertBefore(gads, node);
         })();
