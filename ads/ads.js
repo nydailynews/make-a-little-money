910b80b664e30e2a3bd51caf86df162eda18339e
freejoe76 <jmurphy@denverpost.com>
2017-11-09 14:20:18 -0500

    Fix for non-standard footer's

--- ads/ads.js
+++ ads/ads.js
@@ -117,6 +117,7 @@ window.onload = function() {
 
         // Append
         var el = document.getElementById('templatefooter');
+        if ( el === null ) el = document.querySelectorAll('footer')[0];
         el.insertAdjacentHTML('beforebegin', '<div id="taboola-interactive_pages-3x3" style="max-width: 800px; margin: auto"></div>');
 
         // Fire
