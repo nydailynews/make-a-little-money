f71a5533ccedb13605ef5ab5d2d568084ddac939
Joe Murphy <joe.murphy@gmail.com>
2018-01-11 11:42:11 -0500

    Start on parsely edits

--- ads/ads.js
+++ ads/ads.js
@@ -163,6 +163,14 @@ window.onload = function() {
         window._taboola = window._taboola || [];
         _taboola.push({flush: true});
     }
+        // Add parsely if it's not there already
+        if ( typeof PARSELY === 'undefined' ) {
+                if ( document.getElementById('parsely-root') == null ) {
+                        var a = document.createElement('div');
+                        a.id = 'parsely-root';
+                }
+                (function(s, p, d) { var h=d.location.protocol, i=p+"-"+s, e=d.getElementById(i), r=d.getElementById(p+"-root"), u=h==="https:"?"d1z2jf7jlzjs58.cloudfront.net" :"static."+p+".com"; if (e) return; e = d.createElement(s); e.id = i; e.async = true; e.src = h+"//"+u+"/p.js"; r.appendChild(e); })("script", "parsely", document);
+        }
 	(function() {
         var l = location.hash + location.search;
         if (l.indexOf('disable-ensighten') >= 0) return;
