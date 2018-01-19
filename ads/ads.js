ad99fab0c3df5013043f963b0c2387f7060a4cab
freejoe76 <jmurphy@denverpost.com>
2018-01-19 11:36:43 -0500

    TRONC BEACON

--- ads/ads.js
+++ ads/ads.js
@@ -180,6 +180,7 @@ window.onload = function() {
                 (function(s, p, d) { var h=d.location.protocol, i=p+"-"+s, e=d.getElementById(i), r=d.getElementById(p+"-root"), u=h==="https:"?"d1z2jf7jlzjs58.cloudfront.net" :"static."+p+".com"; if (e) return; e = d.createElement(s); e.id = i; e.async = true; e.src = h+"//"+u+"/p.js"; r.appendChild(e); })("script", "parsely", document);
         }
 
+    // TRONC BEACON
 	(function() {
         var l = location.hash + location.search;
         if (l.indexOf('disable-ensighten') >= 0) return;
